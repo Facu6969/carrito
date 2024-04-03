8b3812395f86ea589ec2592f267d8113d3008b75
facu-87 <facu.fernandez87@gmail.com>
2024-04-02 23:22:32 -0300

    Agregando JS al shop

---
+++ JS/producto.js
@@ -0,0 +1,34 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "remera",
+      precio: 1550,
+      img:
+        "https://http2.mlstatic.com/D_NQ_NP_924954-MLA44509209660_012021-O.webp",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      nombre: "buzo",
+      precio: 5000,
+      img:
+        "https://acdn.mitiendanube.com/stores/440/495/products/gato-astronauta-hoodie-111-897b94cf34cede53fc16272219971334-1024-1024.webp",
+      cantidad: 1,
+    },
+    {
+      id: 3,
+      nombre: "musculosa",
+      precio: 1000,
+      img:
+        "https://acdn.mitiendanube.com/stores/358/245/products/tripmiscelanea4-6da9d06965a5ce634417052685294232-640-0.webp",
+      cantidad: 1,
+    },
+    {
+      id: 4,
+      nombre: "pantalon",
+      precio: 4000,
+      img:
+        "https://m.media-amazon.com/images/I/51Ar-J18z9L._AC_SX569_.jpg",
+      cantidad: 1,
+    },
+  ];
